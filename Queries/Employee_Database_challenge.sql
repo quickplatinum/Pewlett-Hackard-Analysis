19e049b850497e9b261e3ccc827560d13930c37c
quickplatinum <88692025+quickplatinum@users.noreply.github.com>
2021-10-17 19:15:49 -0400

    Module 7 SQL Challenge

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -34,3 +34,24 @@ GROUP BY title
 ORDER BY count DESC;
 
 SELECT * FROM retiring_titles
+
+-- Deleiverable 2
+-- Mentorship Eligibilty Table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+	FROM employees as e
+		INNER JOIN dept_emp as de
+			ON (e.emp_no = de.emp_no)
+		INNER JOIN titles as t
+			ON (de.emp_no = t.emp_no)
+WHERE de.to_date = ('9999-01-01') 
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibilty
