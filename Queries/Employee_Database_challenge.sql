3b5a05eca1b2272410709d3526f06bbe8aeab5d4
quickplatinum <88692025+quickplatinum@users.noreply.github.com>
2021-10-17 19:02:37 -0400

    Module 7 SQL Challenge

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,36 +1,36 @@
 -- Deleiverable 1
--- Create a new table called retirement_titles
-SELECT 
-e.emp_no, e.first_name, e.last_name, 
-t.title, t.from_date, t.to_date
+-- Create a new table called retirement titles table
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+    t.title,
+    t.from_date,
+    t.to_date
 INTO retirement_titles
-FROM employees as e
-LEFT JOIN titles as t
-ON (e.emp_no = t.emp_no)
+FROM employees AS e
+    INNER JOIN titles as t
+        ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY e.emp_no, title;
+ORDER BY e.emp_no;
 
---Show Result of retirement titles
 SELECT * FROM retirement_titles
 
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) 
-emp_no, first_name, last_name, title
+-- Remove Duplicate rows and create unique titles table
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
 INTO unique_titles
 FROM retirement_titles
-ORDER BY emp_no, title DESC;
+ORDER BY emp_no, to_date DESC;
 
---Show Result of unique titles
 SELECT * FROM unique_titles
 
--- Addditon of all retiring employees using Count fucntion into new table 
-SELECT COUNT(unique_titles), title
+-- Additon of all retiring employees using Count fucntion into new retiring titles table 
+SELECT COUNT(title), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
-ORDER BY count desc;
+ORDER BY count DESC;
 
---Show Result of retiring titles
 SELECT * FROM retiring_titles
-
-
