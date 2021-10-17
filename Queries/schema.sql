49ba06640dd2035f96ac2bb4db42b80520514107
quickplatinum <88692025+quickplatinum@users.noreply.github.com>
2021-10-17 16:13:28 -0400

    Add files via upload

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,48 +1,3 @@
--- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
-	dept_no VARCHAR(4) NOT NULL,
-	dept_name VARCHAR(40) NOT NULL,
-	PRIMARY KEY (dept_no),
-	UNIQUE (dept_name)
-);
-create table dept_emp (
-	emp_no int not null,
-	dept_no varchar not null,
-	from_date date not null,
-	to_date date not null,
-	foreign key (emp_no) references employees (emp_no),
-	foreign key (dept_no) references departments (dept_no),
-	primary key (emp_no, dept_no)
-);
-create table employees (
-	emp_no int not null,
-	birth_date date not null,
-	first_name varchar not null,
-	last_name varchar not null,
-	gender varchar not null,
-	hire_date date not null,
-	primary key (emp_no)
-);
-create table dept_manager (
-	dept_no varchar not null,
-	emp_no int not null,
-	from_date date not null,
-	to_date date not null,
-	foreign key (emp_no) references employees (emp_no),
-	foreign key (dept_no) references departments (dept_no),
-	primary key (emp_no, dept_no)
-);
-create table salaries (
-	emp_no int not null,
-	salary int not null,
-	from_date date not null,
-	to_date date not null,
-	foreign key (emp_no) references employees (emp_no),
-	primary key (emp_no)
-);
-create table titles (
-	emp_no int not null,
-	title varchar not null,
 -- Joining departments and dept_manager tables
 SELECT departments.dept_name,
      dept_manager.emp_no,
@@ -96,3 +51,53 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+
+SELECT * FROM salaries;
+SELECT * FROM emp_info;
+
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	      AND (de.to_date = '9999-01-01');
+		  
+SELECT * FROM emp_info; 
+	
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+	
+select * from manager_info
+	
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+	FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
